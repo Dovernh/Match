7d48d230204e151f10e58f6da8ef565df5748b2c
Dovernh <dovernh@gmail.com>
2021-06-29 11:23:10 -0400

    styling, wording, definition changes

--- src/app/match/match.component.ts
+++ src/app/match/match.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Definition } from '../models/definition.model';
 import { Word } from '../models/word.model';
@@ -8,148 +15,152 @@ import { Word } from '../models/word.model';
   templateUrl: './match.component.html',
   styleUrls: ['./match.component.scss'],
 })
-export class MatchComponent implements OnInit {
+export class MatchComponent implements OnInit, AfterViewInit {
+  @ViewChild('headerContainer') headerContainer!: ElementRef;
+  @ViewChild('defContainer') defContainer!: ElementRef;
+
   selectedWord: string | null = null;
 
   words: Word[] = [
-    { displayName: 'Deserunt', selected: false, assigned: false },
-    { displayName: 'Consequuntur', selected: false, assigned: false },
-    { displayName: 'Explicabo', selected: false, assigned: false },
-    { displayName: 'Reiciendis', selected: false, assigned: false },
-    { displayName: 'Perspiciatis', selected: false, assigned: false },
-    { displayName: 'Necessitatibus', selected: false, assigned: false },
-    { displayName: 'Repudiandae', selected: false, assigned: false },
-    { displayName: 'Cumque', selected: false, assigned: false },
-    { displayName: 'Esse', selected: false, assigned: false },
-    { displayName: 'Cupiditate', selected: false, assigned: false },
-    { displayName: 'Blanditiis ', selected: false, assigned: false },
-    { displayName: 'Temporibus', selected: false, assigned: false },
-    { displayName: 'Quaerat', selected: false, assigned: false },
-    { displayName: 'Laboriosam', selected: false, assigned: false },
-    { displayName: 'Architecto', selected: false, assigned: false },
-    { displayName: 'Repellendus', selected: false, assigned: false },
-    { displayName: 'Numquam', selected: false, assigned: false },
-    { displayName: 'Cupiditate', selected: false, assigned: false },
-    { displayName: 'Upiditate', selected: false, assigned: false },
-    { displayName: 'Voluptatem', selected: false, assigned: false },
+    { displayName: 'coefficient', selected: false, assigned: false },
+    { displayName: 'constant', selected: false, assigned: false },
+    { displayName: 'domain', selected: false, assigned: false },
+    { displayName: 'equation', selected: false, assigned: false },
+    { displayName: 'expression', selected: false, assigned: false },
+    { displayName: 'factors', selected: false, assigned: false },
+    { displayName: 'function', selected: false, assigned: false },
+    { displayName: 'inequality', selected: false, assigned: false },
+    { displayName: 'integers', selected: false, assigned: false },
+    { displayName: 'inverse', selected: false, assigned: false },
+    { displayName: 'like terms ', selected: false, assigned: false },
+    { displayName: 'linear equation', selected: false, assigned: false },
+    { displayName: 'perfect square', selected: false, assigned: false },
+    { displayName: 'point', selected: false, assigned: false },
+    { displayName: 'quadratic equation', selected: false, assigned: false },
+    { displayName: 'range', selected: false, assigned: false },
+    { displayName: 'reciprocals', selected: false, assigned: false },
+    { displayName: 'relation', selected: false, assigned: false },
+    { displayName: 'roots', selected: false, assigned: false },
+    { displayName: 'slope', selected: false, assigned: false },
   ];
 
   defs: Definition[] = [
     {
-      definition:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt velit perferendis. Sapiente vel eum harum.',
+      definition: 'product of two numbers is 1',
+      assignedWord: '',
+    },
+    {
+      definition: 'lines and sets that never end',
+      assignedWord: '',
+    },
+    {
+      definition: 'positive or negative whole number or zero',
+      assignedWord: '',
+    },
+    {
+      definition: 'square root is a rational number',
+      assignedWord: '',
+    },
+    {
+      definition: 'intersection of two lines',
+      assignedWord: '',
+    },
+    {
+      definition: 'used when exact answer is unknown',
+      assignedWord: '',
+    },
+    {
+      definition: 'number in front of variable',
+      assignedWord: '',
+    },
+    {
+      definition: 'variables to second power',
+      assignedWord: '',
+    },
+    {
+      definition: 'value that never changes',
+      assignedWord: '',
+    },
+    {
+      definition: 'graph is straight line',
+      assignedWord: '',
+    },
+    {
+      definition: 'ratio of change in y to corresponding change in x',
+      assignedWord: '',
+    },
+    {
+      definition: 'values of independent variable',
+      assignedWord: '',
+    },
+    {
+      definition: 'one operation undoes effect of other operation',
       assignedWord: '',
     },
     {
       definition:
-        'est reiciendis quo magnam iure. Reiciendis numquam autem deleniti suscipit, cum eum eveniet magni tenetur',
+        'mathematical sentence stating that two expressions are equal',
+      assignedWord: '',
+    },
+    {
+      definition: 'solution for a quadratic equation',
+      assignedWord: '',
+    },
+    {
+      definition: 'quantities being multiplied',
+      assignedWord: '',
+    },
+    {
+      definition: 'values for dependent variable',
+      assignedWord: '',
+    },
+    {
+      definition: 'value in input has exactly one value in output',
+      assignedWord: '',
+    },
+    {
+      definition: 'contain same variables raised to same power',
+      assignedWord: '',
+    },
+    {
+      definition: 'rectangular arrangement of numbers in rows and columns',
       assignedWord: '',
     },
     {
       definition:
-        'odit doloremque accusamus doloribus quibusdam repellat distinctio consequuntur? Quis fugiat omnis, aliquam sunt nulla nesciunt',
+        'combines numbers and/or variables using mathematical operations',
       assignedWord: '',
     },
     {
-      definition:
-        'Minus dolorem illum, vero est voluptates adipisci, exercitationem in laudantium a maiores dolore deleniti, quaerat',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'placeat rerum. Quisquam quas quibusdam accusantium. Accusamus earum architecto corporis optio maiores, blanditiis omnis cux!',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Deserunt, ad eveniet expedita suscipit ratione, perferendis esse facilis, omnis iste eius fuga ea quibusdam',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'natus debitis ducimus dicta cumque beatae culpa numquam nisi possimus asperiores? Cupiditate corporis asperiores vitae.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Repudiandae quasi quam nemo rerum necessitatibus molestiae eaque aspernatur odio laborum, reiciendis minima illo perspiciatis',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'tempore ea consequuntur ad sequi impedit. Vitae iure, quas recusandae labore numquam facere? Odit, fuga.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Libero suscipit voluptates recusandae non nisi assumenda enim sed dignissimos commodi, minima ipsa illo totam',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Liste quo. In hic, nesciunt, tempora sunt repudiandae voluptatum perspiciatis accusantium aperiam ullam at ea.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Non, illum dicta. Asperiores culpa sunt ullam inventore saepe doloribus quae itaque maxime quo in',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Hic accusantium deserunt labore dolorem similique, dolore repudiandae quis, ratione sapiente soluta error consectetur adipisci.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Earum minima natus mollitia repellendus nemo est deleniti quisquam eligendi nobis. Ratione et in beatae',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Cupiditate, blanditiis nihil voluptatum quisquam, assumenda sunt voluptas eius voluptates doloremque, inventore consequatur id hic.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Eos aperiam eligendi est, repudiandae voluptatem, sed temporibus ut ratione molestias et numquam amet fugit',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Obcaecati, quisquam ipsam recusandae aspernatur iste eum! Hic omnis rem est consequatur ut incidunt et.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Obcaecati eos, consequatur consectetur sint deserunt, saepe ea, facilis adipisci praesentium nam sunt. Fugit incidunt',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Quo voluptas, ea, earum non ut, deleniti odio velit consectetur totam eius. Fugit, magnam praesentium?',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Eaque fuga cupiditate, at ex tenetur mollitia iste dolore? Quas consequuntur porro corrupti blanditiis illo',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Asperiores est distinctio perspiciatis, eum doloremque? Repellendus autem atque, voluptate nesciunt veritatis totam dolorum eius.',
-      assignedWord: '',
-    },
-    {
-      definition:
-        'Itaque reiciendis, aspernatur quasi ratione sum, magnam blanditiis atque accusantium, cupiditate nobis in vel eum',
+      definition: 'set of ordered pairs',
       assignedWord: '',
     },
   ];
 
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.words.sort((a, b) =>
+      a.displayName > b.displayName ? 1 : b.displayName < a.displayName ? -1 : 0
+    );
+  }
+
+  ngAfterViewInit(): void {
+    this.adjustDefContainerHeight();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.adjustDefContainerHeight();
+  }
+
+  adjustDefContainerHeight() {
+    const headerHeight = this.headerContainer.nativeElement as HTMLElement;
+
+    (this.defContainer.nativeElement as HTMLElement).style.marginTop = `${
+      headerHeight.clientHeight + 20
+    }px`;
+  }
 
   selectWord(word: string): void {
     this.selectedWord = null;
